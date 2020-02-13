bc2f01ae7c74ef0692fc4684a972337aa79d73ce
Cassio Scofield <cassio.scofield@ibm.com>
2020-02-13 15:12:43 -0300

    Solucao

---
+++ caixa.js
@@ -0,0 +1,37 @@
+module.exports = {
+  getNotas
+};
+
+
+function isValorDivisivelPor10(valor) {
+  return (valor % 10) == 0;
+}
+
+function getNotas(valor) {
+
+  const notasDisponivel = [100, 50, 20, 10];
+
+  let quantidadeDeNotas=[];
+
+  let i = 0;
+  if (valor == 0) {
+
+  }
+
+  if ((isValorDivisivelPor10(valor) && (valor > 0))) {
+    while (valor>0) {
+      if (valor >= notasDisponivel[i]) {
+        valor = valor - notasDisponivel[i];
+        quantidadeDeNotas.push(notasDisponivel[i]);
+      } else {
+        i++;
+      }
+    }
+  } else if (valor == 0) {
+    quantidadeDeNotas = [];
+  } else {
+    quantidadeDeNotas = null;
+  }
+
+  return quantidadeDeNotas;
+}
